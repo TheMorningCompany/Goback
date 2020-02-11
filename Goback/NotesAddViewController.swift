557489e181e45edad1b57605cbf59c829a493852
Zeqe G <zeqe.g@icloud.com>
2020-02-10 22:10:09 -0800

    viewcontrollers for the notes things

---
+++ Goback/NotesAddViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NotesAddViewController.swift
+//  Goback
+//
+//  Created by Zeqiel Golomb on 2/10/20.
+//  Copyright © 2020 Zeqe Golomb. All rights reserved.
+//
+
+import UIKit
+
+class NotesAddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
