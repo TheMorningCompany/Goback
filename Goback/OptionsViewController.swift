a99b44ba530a3b21cb0f0148d5231e9e82af0bf5
Zeqe G <zeqe.g@icloud.com>
2020-03-22 12:57:33 -0700

    fixed app icons and options

--- Goback/OptionsViewController.swift
+++ Goback/OptionsViewController.swift
@@ -18,36 +18,8 @@ class OptionsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    func doHaptic() {
-        if (UserDefaults.standard.bool(forKey: "enable_haptics")) {
-            impact.impactOccurred()
-        }
-    }
+    
 
-
-    @IBAction func didTapLightIcon(_ sender: UIButton) {
-        doHaptic()
-        UIApplication.shared.setAlternateIconName("green")
-    }
-    @IBAction func didTapDarkIcon(_ sender: UIButton) {
-        doHaptic()
-        UIApplication.shared.setAlternateIconName("dark")
-    }
-    @IBAction func didTapGreenIcon(_ sender: UIButton) {
-        doHaptic()
-        UIApplication.shared.setAlternateIconName(nil)
-    }
-    @IBAction func didTapShadowIcon(_ sender: UIButton) {
-        doHaptic()
-        UIApplication.shared.setAlternateIconName("shadow")
-    }
-    @IBAction func didTapGlyphIcon(_ sender: UIButton) {
-        doHaptic()
-        UIApplication.shared.setAlternateIconName("glyph")
-    }
-    @IBAction func didTapFadeIcon(_ sender: UIButton) {
-        doHaptic()
-        UIApplication.shared.setAlternateIconName("fade")
-    }
+    
 
 }
