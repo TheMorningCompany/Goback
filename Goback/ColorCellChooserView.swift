e7017e4e6e3a6794f32700ad84343e0f287b320d
Zeqe G <zeqe.g@icloud.com>
2020-11-01 18:29:58 -0800

    color picker in todo tab, needs coding

---
+++ Goback/ColorCellChooserView.swift
@@ -0,0 +1,17 @@
+//
+//  ColorCellChooserView.swift
+//  Goback
+//
+//  Created by Zeqiel Golomb on 11/1/20.
+//  Copyright © 2020 Zeqe Golomb. All rights reserved.
+//
+
+import UIKit
+
+class ColorCellChooserView: UIView {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = frame.size.height / 2
+    }
+
+}
