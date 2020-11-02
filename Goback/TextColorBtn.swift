51e2f22783dd091c67ac2754339a60e3db69a2d9
Zeqe G <zeqe.g@icloud.com>
2020-11-02 09:59:24 -0800

    text color ui tweaks

---
+++ Goback/TextColorBtn.swift
@@ -0,0 +1,18 @@
+//
+//  TextColorBtn.swift
+//  Goback
+//
+//  Created by Zeqiel Golomb on 11/2/20.
+//  Copyright © 2020 Zeqe Golomb. All rights reserved.
+//
+
+import UIKit
+
+class TextColorBtn: UIButton {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 20
+        layer.cornerCurve = .continuous
+    }
+
+}
